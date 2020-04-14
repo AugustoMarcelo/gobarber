ebe94af126b5ef89c46427f9fe5dba4f76dad450
AugustoMarcelo <mrclgst10@gmail.com>
2020-04-13 23:33:19 -0300

    Adicionando campo default

--- backend/src/database/migrations/1586827217978-CreateAppointments.ts
+++ backend/src/database/migrations/1586827217978-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1586827217978
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
