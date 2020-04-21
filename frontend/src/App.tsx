e84221103d9f1648406ca5c0b2014df46e87a55a
AugustoMarcelo <mrclgst10@gmail.com>
2020-04-21 13:36:16 -0300

    Finalizando estilização da página de login

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SignIn from './pages/SignIn';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
