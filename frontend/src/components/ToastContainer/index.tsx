3eac21328e8eed3818c55d75a0151fb55df4f0d5
AugustoMarcelo <mrclgst10@gmail.com>
2020-04-21 20:19:34 -0300

    Criado componente Toast para controlar remoção

---
+++ frontend/src/components/ToastContainer/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Toast from './Toast';
+
+import { ToastMessage } from '../../hooks/toast';
+import { Container } from './styles';
+
+interface ToastContainerProps {
+  messages: ToastMessage[];
+}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  return (
+    <Container>
+      {messages.map(message => (
+        <Toast key={message.id} message={message} />
+      ))}
+    </Container>
+  );
+};
+
+export default ToastContainer;
