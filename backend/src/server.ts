33c50c6cbc0fe4a1f2a75aff62ba104d899bb25d
AugustoMarcelo <mrclgst10@gmail.com>
2020-04-22 14:42:11 -0300

    Corrigindo falta de return

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,7 +18,7 @@ app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
-    response
+    return response
       .status(err.statusCode)
       .json({ status: 'error', message: err.message });
   }
