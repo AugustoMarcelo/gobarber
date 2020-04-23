4d671e93305a34858510d307760f458221ab7949
AugustoMarcelo <mrclgst10@gmail.com>
2020-04-23 19:28:07 -0300

    Adicionando estilização para validação de campo

--- mobile/src/components/Input/index.tsx
+++ mobile/src/components/Input/index.tsx
@@ -69,7 +69,7 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container isFocused={isFocused}>
+    <Container isFocused={isFocused} isErrored={!!error}>
       <Icon
         name={icon}
         size={18}
