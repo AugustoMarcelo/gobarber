6788d27c4eb297359d9e869bfa23de44cb5d816a
AugustoMarcelo <mrclgst10@gmail.com>
2020-04-23 20:12:34 -0300

    Importando provider com contexto da aplicação

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+import AppProvider from './hooks';
+
 import Routes from './routes';
 
 const App: React.FC = () => (
   <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <View style={{ backgroundColor: '#312e38', flex: 1 }}>
-      <Routes />
-    </View>
+    <AppProvider>
+      <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+        <Routes />
+      </View>
+    </AppProvider>
   </NavigationContainer>
 );
 
