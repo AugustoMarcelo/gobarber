b49b59d3ff5fe76903f48bec0c271135568b4718
AugustoMarcelo <mrclgst10@gmail.com>
2020-04-23 13:51:57 -0300

    Usando fontes customizadas

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Image } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
     </Container>
   );
 };
