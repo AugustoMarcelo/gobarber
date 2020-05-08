2b85b7f992d6e85dd4352a8160a3257e5bb41b85
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-08 15:58:43 -0300

    Criado provider de email com Ethereal

---
+++ backend/src/shared/container/providers/MailProvider/dtos/ISendMailDTO.ts
@@ -0,0 +1,13 @@
+import IParseEmailTemplateDTO from '@shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO';
+
+interface IMailContact {
+  name: string;
+  email: string;
+}
+
+export default interface ISendMailDTO {
+  to: IMailContact;
+  from?: IMailContact;
+  subject: string;
+  templateData: IParseEmailTemplateDTO;
+}
