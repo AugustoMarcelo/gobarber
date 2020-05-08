9e9e6a956232e652b045aab0482cf6e6581e4ed0
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-07 21:56:45 -0300

    Adicionado await antes do expect

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -31,7 +31,7 @@ describe('CreateAppointment', () => {
       provider_id: '123',
     });
 
-    expect(
+    await expect(
       createAppointment.execute({
         date: appointmentDate,
         provider_id: '123',
