b277bd7d7b2388ef8e46e26ec4f5ec47e5ff5b81
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-09 14:16:08 -0300

    Adicionada rota de fornecedores de serviços

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
@@ -10,6 +11,7 @@ const routes = Router();
 
 routes.use('/sessions', sessionsRouter);
 routes.use('/appointments', appointmentsRouter);
+routes.use('/providers', providersRouter);
 routes.use('/users', usersRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
