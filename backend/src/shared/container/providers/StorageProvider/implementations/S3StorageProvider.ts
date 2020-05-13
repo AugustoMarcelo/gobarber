175eae6f9deb5b5f5b06e1ad0ca96c2052a11d87
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-13 20:17:27 -0300

    Implementado upload de arquivos via Amazon S3

---
+++ backend/src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import path from 'path';
+import mime from 'mime';
+import aws, { S3 } from 'aws-sdk';
+import uploadConfig from '@config/upload';
+import IStorageProvider from '../models/IStorageProvider';
+
+class S3StorageProvider implements IStorageProvider {
+  private client: S3;
+
+  constructor() {
+    this.client = new aws.S3({
+      region: process.env.AWS_DEFAULT_REGION,
+    });
+  }
+
+  public async saveFile(file: string): Promise<string> {
+    const originalPath = path.resolve(uploadConfig.tmpFolder, file);
+
+    const ContentType = mime.getType(originalPath);
+
+    if (!ContentType) {
+      throw new Error('File not found');
+    }
+
+    const fileContent = await fs.promises.readFile(originalPath);
+
+    await this.client
+      .putObject({
+        Bucket: uploadConfig.config.aws.bucket,
+        Key: file,
+        ACL: 'public-read',
+        Body: fileContent,
+        ContentType,
+        ContentDisposition: `inline; filename=${file}`,
+      })
+      .promise();
+
+    await fs.promises.unlink(originalPath);
+
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    await this.client
+      .deleteObject({
+        Bucket: uploadConfig.config.aws.bucket,
+        Key: file,
+      })
+      .promise();
+  }
+}
+
+export default S3StorageProvider;
