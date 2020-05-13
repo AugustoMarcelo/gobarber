ef67d9baca437e523271f1b4a7221e5da17b6b69
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-13 10:51:35 -0300

    Retornando url do avatar e excluindo password

--- backend/src/modules/users/infra/http/controllers/UsersController.ts
+++ backend/src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
 
@@ -10,8 +11,6 @@ export default class UsersController {
 
     const user = await createUser.execute({ name, email, password });
 
-    delete user.password;
-
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
