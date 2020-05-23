b98a2f85d0a664667bd48213df950e6f2d6344a1
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-23 19:14:58 -0300

    Ajustando rota de recuperação de senha

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -82,9 +82,9 @@ const SignIn: React.FC = () => {
 
             <Button type="submit">Entrar</Button>
 
-            <a href="forgot">Esqueci minha senha</a>
+            <Link to="/forgot-password">Esqueci minha senha</Link>
           </Form>
-          <Link to="signup">
+          <Link to="/signup">
             <FiLogIn />
             Criar conta
           </Link>
