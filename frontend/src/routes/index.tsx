09e9499373d7865ae4a8f24eec4c7e833f9fc697
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-23 19:14:17 -0300

    Importadas as rotas de perfil, e recuperação de senha

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,14 +5,21 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
+
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
+    <Route path="/profile" component={Profile} isPrivate />
   </Switch>
 );
 
